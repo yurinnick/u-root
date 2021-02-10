db1a55503b9f9bc846012d49dc9698b827e413c1
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-02-10 14:49:49 +0100

    tools/stmanager: add keygen subcmd to generate ed25519 keys and certs
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- tools/stmanager/commands.go
+++ tools/stmanager/commands.go
@@ -5,14 +5,23 @@
 package main
 
 import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math/big"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/u-root/u-root/pkg/boot/stboot"
 )
 
-func packOSPackage(out, label, pkgURL, kernel, initramfs, cmdline, tboot, tbootArgs string, acms []string) error {
+func createCmd(out, label, pkgURL, kernel, initramfs, cmdline, tboot, tbootArgs string, acms []string) error {
 	ospkg, err := stboot.InitOSPackage(out, label, pkgURL, kernel, initramfs, cmdline, tboot, tbootArgs, acms)
 	if err != nil {
 		return err
@@ -28,15 +37,22 @@ func packOSPackage(out, label, pkgURL, kernel, initramfs, cmdline, tboot, tbootA
 	return nil
 }
 
-func addSignatureToOSPackage(osPackage, privKey, cert string) error {
-	ospkg, err := stboot.OSPackageFromFile(osPackage)
+func signCmd(osPackagePath, privKeyPath, certPath string) error {
+	ospkg, err := stboot.OSPackageFromFile(osPackagePath)
 	if err != nil {
 		return err
 	}
 
-	log.Print("Signing OS package ...")
-	log.Printf("private key: %s", privKey)
-	log.Printf("certificate: %s", cert)
+	privKey, err := loadPEM(privKeyPath)
+	if err != nil {
+		return err
+	}
+
+	cert, err := loadPEM(certPath)
+	if err != nil {
+		return err
+	}
+
 	err = ospkg.Sign(privKey, cert)
 	if err != nil {
 		return err
@@ -50,7 +66,7 @@ func addSignatureToOSPackage(osPackage, privKey, cert string) error {
 	return nil
 }
 
-func unpackOSPackage(ospkgPath string) error {
+func showCmd(ospkgPath string) error {
 	ospkg, err := stboot.OSPackageFromFile(ospkgPath)
 	if err != nil {
 		return err
@@ -58,4 +74,132 @@ func unpackOSPackage(ospkgPath string) error {
 
 	log.Println("Archive unpacked into: " + ospkg.Archive)
 	return nil
+}
+
+func keygenCmd(rootCertPath, rootKeyPath string, validFrom, validUntil time.Time, certOut, keyOut string) error {
+	var newCert *x509.Certificate
+	var newPriv ed25519.PrivateKey
+	var err error
+
+	if rootCertPath == "" || rootKeyPath == "" {
+		// self signed certificate
+		newCert, newPriv, err = newCertWithED25519Keys(nil, nil, validFrom, validUntil)
+		if err != nil {
+			return fmt.Errorf("keygen: %v", err)
+		}
+
+	} else {
+		rootCertBlock, err := loadPEM(rootCertPath)
+		if err != nil {
+			return fmt.Errorf("keygen: %v", err)
+		}
+		rootCert, err := x509.ParseCertificate(rootCertBlock.Bytes)
+		if err != nil {
+			return fmt.Errorf("keygen: parsing root cert failed: %v", err)
+		}
+
+		rootKeyBlock, err := loadPEM(rootKeyPath)
+		if err != nil {
+			return fmt.Errorf("keygen: %v", err)
+		}
+		rootKey, err := x509.ParsePKCS8PrivateKey(rootKeyBlock.Bytes)
+		if err != nil {
+			return fmt.Errorf("keygen: parsing root key failed: %v", err)
+		}
+
+		newCert, newPriv, err = newCertWithED25519Keys(rootCert, &rootKey, validFrom, validUntil)
+		if err != nil {
+			return fmt.Errorf("keygen: %v", err)
+		}
+	}
+
+	key, err := x509.MarshalPKCS8PrivateKey(newPriv)
+	if err != nil {
+		return fmt.Errorf("failed to marshal private key: %v", err)
+	}
+
+	certBlock := pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: newCert.Raw,
+	}
+
+	keyBlock := pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: key,
+	}
+
+	if err := writePEM(&certBlock, certOut); err != nil {
+		return fmt.Errorf("keygen: failed to write cert: %v", err)
+	}
+
+	if err := writePEM(&keyBlock, keyOut); err != nil {
+		return fmt.Errorf("keygen: failed to write key: %v", err)
+	}
+
+	return nil
+}
+
+func newCertWithED25519Keys(rootCert *x509.Certificate, rootKey *interface{}, validFrom, validUntil time.Time) (*x509.Certificate, ed25519.PrivateKey, error) {
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, nil, fmt.Errorf("new cert: failed to generate serial number: %v", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		NotBefore:    validFrom,
+		NotAfter:     validUntil,
+	}
+
+	newPub, newPriv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, fmt.Errorf("new cert: failed to generate key: %v", err)
+	}
+
+	var certBytes []byte
+
+	if rootCert == nil || rootKey == nil {
+		// creating self signed certificate
+		template.KeyUsage |= x509.KeyUsageCertSign
+		template.BasicConstraintsValid = true
+		template.IsCA = true
+		certBytes, err = x509.CreateCertificate(rand.Reader, &template, &template, newPub, newPriv)
+	} else {
+		// creating certificate signed by root
+		certBytes, err = x509.CreateCertificate(rand.Reader, &template, rootCert, newPub, *rootKey)
+	}
+	if err != nil {
+		return nil, nil, fmt.Errorf("new cert: creation failed: %v", err)
+	}
+
+	newCert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, nil, fmt.Errorf("new cert: parsing failed: %v", err)
+	}
+
+	return newCert, newPriv, nil
+}
+
+func loadPEM(path string) (*pem.Block, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	block, rest := pem.Decode(bytes)
+	if block == nil {
+		return nil, errors.New("no PEM block found")
+	}
+	if len(rest) != 0 {
+		return nil, errors.New("unexpected trailing data after PEM block")
+	}
+	return block, nil
+}
+
+func writePEM(b *pem.Block, path string) error {
+	pemBytes := pem.EncodeToMemory(b)
+	return ioutil.WriteFile(path, pemBytes, 0666)
 }
--- tools/stmanager/stmanager.go
+++ tools/stmanager/stmanager.go
@@ -13,6 +13,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
@@ -23,14 +24,19 @@ const (
 	// HelpText is the command line help
 	HelpText = "stmanager can be used for managing System Transparency OS packages"
 
-	OSPackageDefaultName = "system-transparency-os-package.zip"
+	DefaultPackageName  = "system-transparency-os-package.zip"
+	DefaultCertName     = "cert.pem"
+	DefaultRootCertName = "rootcert.pem"
+	DefaultKeyName      = "key.pem"
+	DefaultRootKeyName  = "rootkey.pem"
+	DateFormat          = "02 Jan 06 15:04 UTC" //time.RFC822
 )
 
 var goversion string
 
 var (
 	create          = kingpin.Command("create", "Create a OS package from the provided operating system files")
-	createOut       = create.Flag("out", "Path to output ZIP archive file.").String()
+	createOut       = create.Flag("out", "Path to output ZIP archive file. Defaults to "+DefaultPackageName).String()
 	createLabel     = create.Flag("label", "Short description of the boot configuration. Defaults to 'System Tarnsparency OS package <kernel>'").String()
 	createPkgURL    = create.Flag("pkg-url", "URL of the OS package in case of network boot mode").String()
 	createKernel    = create.Flag("kernel", "Operation system kernel").Required().ExistingFile()
@@ -40,13 +46,22 @@ var (
 	createTbootArgs = create.Flag("tcmd", "tboot command line").String()
 	createACM       = create.Flag("acm", "Authenticated Code Module for TXT. This can be a path to single ACM or directory containig multiple ACMs.").ExistingFileOrDir()
 
-	sign            = kingpin.Command("sign", "Sign the binary inside the provided OS package")
+	sign            = kingpin.Command("sign", "Sign the provided OS package")
 	signPrivKeyFile = sign.Flag("key", "Private key for signing").Required().ExistingFile()
 	signCertFile    = sign.Flag("cert", "Certificate corresponding to the private key").Required().ExistingFile()
 	signOSPackage   = sign.Arg("OS package", "Archive created by 'stconfig create'").Required().ExistingFile()
 
 	show          = kingpin.Command("show", "Unpack OS package  file into directory")
 	showOSPackage = show.Arg("OS package", "Archive containing the boot files").Required().ExistingFile()
+
+	keygen           = kingpin.Command("keygen", "Generate certificates for signing OS packages using ED25529 keys")
+	keygenRootCert   = kingpin.Flag("rootCert", "Root certificate in PEM format to sign the new certificate. Ignored if --isCA is set").ExistingFile()
+	keygenRootKey    = kingpin.Flag("rootKey", "Root key in PEM format to sign the new certificate. Ignored if --isCA is set").ExistingFile()
+	keygenIsCA       = kingpin.Flag("isCA", "Generate a self signed root certificate.").Bool()
+	keygenValidFrom  = kingpin.Flag("validFrom", "Date formatted as '"+DateFormat+"'. Defaults to time of creation").String()
+	keygenValidUntil = kingpin.Flag("validUntil", "Date formatted as '"+DateFormat+"'. Defaults to time of creation").String()
+	keygenCertOut    = kingpin.Flag("certOut", "Output certificate file. Defaults to "+DefaultCertName+" or "+DefaultRootCertName+" if --isCA is set.").String()
+	keygenKeyOut     = kingpin.Flag("keyOut", "Output key file. Defaults to "+DefaultKeyName+" or "+DefaultRootKeyName+" if --isCA is set.").String()
 )
 
 func main() {
@@ -57,60 +72,106 @@ func main() {
 
 	switch kingpin.Parse() {
 	case create.FullCommand():
-		out, err := checkOutPath(*createOut)
+		checkCreateOut()
+		checkCreateLabel()
+
+		acms, err := checkCreateACMs(*createACM)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		label := checkLabel(*createLabel)
-
-		acms, err := checkACMs(*createACM)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err := packOSPackage(out, label, *createPkgURL, *createKernel, *createInitramfs, *createCmdline, *createTboot, *createTbootArgs, acms); err != nil {
+		if err := createCmd(*createOut, *createLabel, *createPkgURL, *createKernel, *createInitramfs, *createCmdline, *createTboot, *createTbootArgs, acms); err != nil {
 			log.Fatal(err)
 		}
 	case sign.FullCommand():
-		if err := addSignatureToOSPackage(*signOSPackage, *signPrivKeyFile, *signCertFile); err != nil {
+		if err := signCmd(*signOSPackage, *signPrivKeyFile, *signCertFile); err != nil {
 			log.Fatal(err)
 		}
 	case show.FullCommand():
-		if err := unpackOSPackage(*showOSPackage); err != nil {
+		if err := showCmd(*showOSPackage); err != nil {
 			log.Fatal(err)
 		}
+	case keygen.FullCommand():
+		checkKeygenOuts()
+
+		notBefore, err := parseTime(*keygenValidFrom)
+		if err != nil {
+			log.Fatalf("failed to parse 'validFrom' date: %v, try --help", err)
+		}
+		notAfter, err := parseTime(*keygenValidUntil)
+		if err != nil {
+			log.Fatalf("failed to parse 'validUntil' date: %v, try --help", err)
+		}
+
+		if *keygenIsCA {
+			if err := keygenCmd("", "", notBefore, notAfter, *keygenCertOut, *keygenKeyOut); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			if *keygenRootCert == "" || *keygenRootKey == "" {
+				log.Fatal("missing flag, try --help")
+			}
+			if err := keygenCmd(*keygenRootCert, *keygenRootKey, notBefore, notAfter, *keygenCertOut, *keygenKeyOut); err != nil {
+				log.Fatal(err)
+			}
+		}
 	default:
-		log.Fatal("Command not found")
+		log.Fatal("command not found")
 	}
 }
 
-func checkOutPath(out string) (string, error) {
-	if out == "" {
-		out = OSPackageDefaultName
-	}
+func checkCreateOut() {
+	if *createOut == "" {
+		*createOut = DefaultPackageName
+	} else {
+		dir := filepath.Dir(*createOut)
+		if _, err := os.Stat(dir); err != nil {
+			log.Fatal(err)
+		}
 
-	dir := filepath.Dir(out)
-	if _, err := os.Stat(dir); err != nil {
-		return "", err
+		ext := filepath.Ext(*createOut)
+		if ext != ".zip" {
+			*createOut = *createOut + ".zip"
+		}
 	}
-
-	ext := filepath.Ext(out)
-	if ext != ".zip" {
-		out = out + ".zip"
-	}
-	return out, nil
 }
 
-func checkLabel(label string) string {
-	if label == "" {
+func checkKeygenOuts() {
+	if *keygenCertOut == "" {
+		if *keygenIsCA {
+			*keygenCertOut = DefaultRootCertName
+		} else {
+			*keygenCertOut = DefaultCertName
+		}
+	} else {
+		dir := filepath.Dir(*keygenCertOut)
+		if _, err := os.Stat(dir); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if *keygenKeyOut == "" {
+		if *keygenIsCA {
+			*keygenKeyOut = DefaultRootKeyName
+		} else {
+			*keygenKeyOut = DefaultKeyName
+		}
+	} else {
+		dir := filepath.Dir(*keygenKeyOut)
+		if _, err := os.Stat(dir); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func checkCreateLabel() {
+	if *createLabel == "" {
 		k := filepath.Base(*createKernel)
-		return fmt.Sprintf("System Tarnsparency OS Package %s", k)
+		*createLabel = fmt.Sprintf("System Tarnsparency OS Package %s", k)
 	}
-	return label
 }
 
-func checkACMs(acm string) ([]string, error) {
+func checkCreateACMs(acm string) ([]string, error) {
 	var acms []string
 	if *createACM != "" {
 		stat, err := os.Stat(*createACM)
@@ -136,4 +197,11 @@ func checkACMs(acm string) ([]string, error) {
 		}
 	}
 	return acms, nil
+}
+
+func parseTime(date string) (time.Time, error) {
+	if len(date) == 0 {
+		return time.Now(), nil
+	}
+	return time.Parse(DateFormat, date)
 }
